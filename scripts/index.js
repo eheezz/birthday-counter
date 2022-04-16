8462970ce2ea688c30ed9c6ee7dbcbe76ee2f103
eheezz <103794073+eheezz@users.noreply.github.com>
2022-04-16 09:07:46 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 4, 2022',
+  birthdate: 'Apr 04, 2022',
   name: 'Amanda'
 };
 
